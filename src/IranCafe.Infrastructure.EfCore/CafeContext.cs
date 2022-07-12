e114163dbc44d114665d8c921e310eae3410b580
hamidba <balalzadehhamid79@gmail.com>
2022-07-12 09:29:14 +0430

    Add Context
    Cafe Context Added

---
+++ src/IranCafe.Infrastructure.EfCore/CafeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IranCafe.Infrastructure.EfCore
+{
+    public class CafeContext : DbContext
+	{
+		public CafeContext(DbContextOptions<CafeContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(s => s.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Cascade;
+
+            //var assembly = typeof(OperatorMapping).Assembly;
+            //modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+    }
+}
