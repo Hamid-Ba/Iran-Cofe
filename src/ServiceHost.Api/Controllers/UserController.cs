821feab06d928c5f37c269aa950ece0358a39770
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-20 16:22:04 +0430

    Add Edit User Api

---
+++ src/ServiceHost.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Framework.Api;
+using IranCafe.Application.Contract.UserAgg;
+using IranCafe.Application.Contract.UserAgg.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    [Authorize]
+    public class UserController : BaseApiController
+    {
+        private readonly IUserApplication _userApplication;
+
+        public UserController(IUserApplication userApplication) => _userApplication = userApplication;
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(EditUserDto command)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _userApplication.Edit(command);
+                    return result.IsSucceeded ? Ok(result.Message) : BadRequest(result.Message);
+                }
+
+                return BadRequest(ApiResultMessages.ModelStateNotValid);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+    }
+}
