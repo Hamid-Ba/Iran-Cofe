c27e08a5a328364a13ce7e08a20ae6f9314e1d78
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-24 16:18:44 +0430

    Add Get All Provinces Api

---
+++ src/ServiceHost.Api/Controllers/ProvinceController.cs
@@ -0,0 +1,24 @@
+﻿using IranCafe.Application.Contract.SiteEntities.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class ProvinceController : BaseApiController
+    {
+        private readonly IProvinceApplication _provinceApplication;
+
+        public ProvinceController(IProvinceApplication provinceApplication) => _provinceApplication = provinceApplication;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _provinceApplication.GetAll();
+                return Ok(result);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+            
+        }
+    }
+}
