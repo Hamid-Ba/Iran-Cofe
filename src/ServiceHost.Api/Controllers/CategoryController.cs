5a2fbfb2e1b85bba36705e7c8e193e3d691ddaa7
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-25 13:26:09 +0430

    Complete Get All Category By Cafe Id Api

--- src/ServiceHost.Api/Controllers/CategoryController.cs
+++ src/ServiceHost.Api/Controllers/CategoryController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IranCafe.Application.Contract.CafeAgg.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ServiceHost.Api.Controllers
 {
     public class CategoryController : BaseApiController
     {
-        
+        private readonly ICategoryApplication _categoryApplication;
+
+        public CategoryController(ICategoryApplication categoryApplication) => _categoryApplication = categoryApplication;
+
+
+        [HttpGet("{cafeId}")]
+        public async Task<IActionResult> GetAllBy(Guid cafeId)
+        {
+            try
+            {
+                var result = await _categoryApplication.GetAllBy(cafeId);
+                return Ok(result);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
     }
 }
