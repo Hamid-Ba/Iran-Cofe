e152ea8cf45f1e8cffaf58c6c4e6c22ef5bf34ac
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-27 09:44:02 +0430

    Complete Get All Gallery By Cafe Id Api

---
+++ src/ServiceHost.Api/Controllers/GalleryController.cs
@@ -0,0 +1,25 @@
+﻿using IranCafe.Application.Contract.CafeAgg.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class GalleryController : BaseApiController
+    {
+        private readonly IGalleryApplication _galleryApplication;
+
+        public GalleryController(IGalleryApplication galleryApplication) => _galleryApplication = galleryApplication;
+
+        [HttpGet("{cafeId}")]
+        public async Task<IActionResult> GetAllBy(Guid cafeId)
+        {
+            try
+            {
+                var result = await _galleryApplication.GetAllBy(cafeId);
+                return Ok(result);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+
+    }
+}
