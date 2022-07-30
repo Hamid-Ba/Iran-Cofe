fbd2ddbfbd0a99d3a021572427ecde98b47027a2
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-30 11:49:49 +0430

    Complete Get All Menu Items Api

---
+++ src/ServiceHost.Api/Controllers/MenuItemController.cs
@@ -0,0 +1,23 @@
+﻿using IranCafe.Application.Contract.CafeAgg.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class MenuItemController : BaseApiController
+    {
+        private readonly IMenuItemApplication _menuItemApplication;
+
+        public MenuItemController(IMenuItemApplication menuItemApplication) => _menuItemApplication = menuItemApplication;
+
+        [HttpGet("{cafeId}/{categoryId?}")]
+        public async Task<IActionResult> GetlAllBy(Guid cafeId, Guid? categoryId)
+        {
+            try
+            {
+                var result = await _menuItemApplication.GetAllBy(cafeId, categoryId);
+                return Ok(result);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+    }
+}
