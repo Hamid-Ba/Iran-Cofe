f03630e55a41dd81fc2e141d5b48d5f5a7cf5fd5
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-08-03 11:44:54 +0430

    Complete Reserve Api

--- src/IranCafe.Infrastructure.EfCore/Repositories/CafeAgg/ReservationRepository.cs
+++ src/IranCafe.Infrastructure.EfCore/Repositories/CafeAgg/ReservationRepository.cs
@@ -4,11 +4,6 @@ using IranCafe.Application.Contract.CafeAgg;
 using IranCafe.Domain.CafeAgg;
 using IranCafe.Domain.CafeAgg.Contracts;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IranCafe.Infrastructure.EfCore.Repositories.CafeAgg
 {
@@ -18,19 +13,20 @@ namespace IranCafe.Infrastructure.EfCore.Repositories.CafeAgg
 
         public ReservationRepository(CafeContext context) : base(context) => _context = context;
 
-        public async Task<IEnumerable<ReservationDto>> GetAll(Guid cafeId, Guid ownerId) => await _context.Reservations.Select(r => new ReservationDto
-        {
-            Id = r.Id,
-            CafeId = r.CafeId,
-            UserId = r.UserId,
-            FullName = r.FullName,
-            Message = r.Message,
-            Phone = r.Phone,
-            PersianCreationDate = r.CreationDate.ToFarsi(),
-            ReserveHourStr = r.ReserveHourStr,
-            ReserveDateStr = r.ReserveDateStr,
-            ReserveDate = r.ReserveDate,
-        }).AsNoTracking().ToListAsync();
-        
+        public async Task<IEnumerable<ReservationDto>> GetAll(Guid cafeId, Guid ownerId) => await _context.Reservations.Where(c => c.CafeId == cafeId && c.UserId == ownerId)
+            .Select(r => new ReservationDto
+            {
+                Id = r.Id,
+                CafeId = r.CafeId,
+                UserId = r.UserId,
+                FullName = r.FullName,
+                Message = r.Message,
+                Phone = r.Phone,
+                PersianCreationDate = r.CreationDate.ToFarsi(),
+                ReserveHourStr = r.ReserveHourStr,
+                ReserveDateStr = r.ReserveDateStr,
+                ReserveDate = r.ReserveDate,
+            }).AsNoTracking().ToListAsync();
+
     }
 }
---
+++ src/ServiceHost.Api/Controllers/ReservationController.cs
@@ -0,0 +1,41 @@
+﻿using Framework.Api;
+using IranCafe.Application.Contract.CafeAgg;
+using IranCafe.Application.Contract.CafeAgg.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class ReservationController : BaseApiController
+    {
+        private readonly IReservationApplication _reservationApplication;
+
+        public ReservationController(IReservationApplication reservationApplication) => _reservationApplication = reservationApplication;
+
+        [HttpGet("{ownerId}/{cafeId}")]
+        public async Task<IActionResult> GetAll(Guid ownerId,Guid cafeId)
+        {
+            try
+            {
+                var result = await _reservationApplication.GetAll(cafeId, ownerId);
+                return Ok(result);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reserve([FromBody]ReserveDto command)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _reservationApplication.Reserve(command);
+                    return result.IsSucceeded ? Ok(result.Message) : Problem(result.Message);
+                }
+
+                return BadRequest(ApiResultMessages.ModelStateNotValid);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+    }
+}
